db2b3bf4f5b8595632aad22a7e5c629aaaadf4a2
margare-jihua <margaret.jihua@gmail.com>
2020-09-14 15:03:16 -0700

    [BankAccount] Add pin

--- bank.py
+++ bank.py
@@ -1,26 +1,36 @@
 class BankAccount():
-    def __init__(self, kind):
+    def __init__(self, kind, pin):
         self.kind = kind
         self.balance = 0
         self.overdraft_fees = 0
+        self.pin = pin
 
-    def deposite(self, cash):
-        self.balance += cash
+    def deposite(self, amount):
+        self.balance += amount
 
-    def withdraw(self, cash):
-        self.balance -= cash
+    def withdraw(self, amount, pin):
+        if (self.pin == pin):
+            if (self.balance < amount):
+                print("Sorry, you don't have enough balance.")
+            elif (self.balance == amount):
+                print("Your account is now empty.")
+            else:
+                self.balance -= amount
+                print("Your current balance is {}.".format(self.balance))
+            return 
+        else:
+            print("The pin is incorrect")
 
-        if (self.balance < 0):
-            self.overdraft_fees += 35
-        return cash
-
-my_account = BankAccount("saving")
+my_account = BankAccount("saving",778)
 
 my_account.deposite(500)
 print('my {} balance is now: ${}'.format(my_account.kind, my_account.balance))
 
-my_account.withdraw(120)
-print('my {} balance is now: ${}'.format(my_account.kind, my_account.balance))
+my_account.withdraw(120,887)
+# print('my {} balance is now: ${}'.format(my_account.kind, my_account.balance))
 
-my_account.withdraw(1000)
-print('my overdraft fee is {}, my {} balance is now: ${}'.format(my_account.overdraft_fees, my_account.kind, my_account.balance))
+# my_account.withdraw(1000)
+# print('my overdraft fee is {}, my {} balance is now: ${}'.format(my_account.overdraft_fees, my_account.kind, my_account.balance))
+
+my_account.withdraw(1000,778)
+my_account.withdraw(50,778)
